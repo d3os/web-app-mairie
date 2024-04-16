8c0d79f68c9a9c3774ada3d93570c5e6f659b461
d3os <121951592+d3os@users.noreply.github.com>
2024-04-16 13:58:03 +0200

    footer into base & menu controller

---
+++ assets/controllers/menu_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from '@hotwired/stimulus';
+
+/* 
+* element with a data-controller="menu" attribute will execute
+* menu_controller.js -> "menu"
+*/ 
+export default class extends Controller {
+
+    connect() {
+    }
+
+    clean(){
+        
+    }
+
+}
