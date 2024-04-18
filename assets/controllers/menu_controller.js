4718364955cd81b3cd696d7d2a89dfd717bc24af
d3os <121951592+d3os@users.noreply.github.com>
2024-04-18 07:20:29 +0200

    changes on menu controller

--- assets/controllers/menu_controller.js
+++ assets/controllers/menu_controller.js
@@ -6,11 +6,27 @@ import { Controller } from '@hotwired/stimulus';
 */ 
 export default class extends Controller {
 
+    mainHtmlContent;
+    mainRefObject;    
+
+    initialize(){ 
+    }
     connect() {
+        this.mainRefObject = document.getElementsByTagName("main")[0];
+    }
+    
+    
+    extractMain(){
+        this.mainHtmlContent = this.mainRefObject.innerHTML;
     }
 
-    clean(){
-        
+    setMainToMenu(){
+        this.extractMain();
+        this.mainRefObject.innerHTML = "<twig:Menu />";
+    }
+
+    setMainToContent(){
+        this.mainRefObject.innerHTML = this.mainHtmlContent;
     }
 
 }
